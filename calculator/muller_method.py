17340f8ba61335cbcdae6ccced38be1a4c976fc9
mhwahdab <moustafawahdan55@gmail.com>
2021-11-21 14:32:01 +0200

    Finished adding complex calculations features

--- calculator/muller_method.py
+++ calculator/muller_method.py
@@ -43,24 +43,25 @@ def iterate(p0, p1, p2, equation, precision, iteration_number):
     p3 = round(p3.real, precision) + round(p3.imag, precision) * 1j if isinstance(p3, complex) else round(p3, precision)
     str_p3 = '(' + str(p3.real) + ('+' if p3.imag > 0 else '-') + str(p3.imag) + 'j)' if isinstance(p3, complex) else str(p3)
     fp3 = eval(equation.replace('$', str_p3))
+    fp3 = round(fp3.real, precision) + round(fp3.imag, precision) * 1j if isinstance(fp3, complex) else round(fp3, precision)
     error = abs(p3 - p2)
     return {
-        "h0": h0,
-        "h1": h1,
-        "sigma0": sigma0,
-        "sigma1": sigma1,
-        "p0": p0,
-        "p1": p1,
-        "p2": p2,
-        "p3": p3,
-        "fp0": fp0,
-        "fp1": fp1,
-        "fp2": fp2,
-        "fp3": fp3,
-        "a": a,
-        "b": b,
-        "c": c,
-        "error": error.real,
+        "h0": h0.real if h0.imag == 0 else h0,
+        "h1": h1.real if h1.imag == 0 else h0,
+        "sigma0": sigma0.real if sigma0.imag == 0 else sigma0,
+        "sigma1": sigma1.real if sigma1.imag == 0 else sigma1,
+        "p0": p0.real if p0.imag == 0 else p0,
+        "p1": p1.real if p1.imag == 0 else p1,
+        "p2": p2.real if p2.imag == 0 else p2,
+        "p3": p3.real if p3.imag == 0 else p3,
+        "fp0": fp0.real if fp0.imag == 0 else fp0,
+        "fp1": fp1.real if fp1.imag == 0 else fp1,
+        "fp2": fp2.real if fp2.imag == 0 else fp2,
+        "fp3": fp3.real if fp3.imag == 0 else fp3,
+        "a": a.real if a.imag == 0 else a,
+        "b": b.real if b.imag == 0 else b,
+        "c": c.real if c.imag == 0 else c,
+        "error": round(error.real, precision),
         "number": iteration_number
     }
 
